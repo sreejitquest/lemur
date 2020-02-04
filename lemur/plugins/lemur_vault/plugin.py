dd66ca0c857a87230b872b6985bda66e56ec3b1c
Sreejit <sreejit.peethambaran@quest-global.com>
2020-02-04 16:54:06 +0530

    fix

--- lemur/plugins/lemur_vault/plugin.py
+++ lemur/plugins/lemur_vault/plugin.py
@@ -264,7 +264,7 @@ class VaultIssuerPlugin(IssuerPlugin):
     def revoke_certificate(self, certificate, comments):
         """Revoke a Vault certificate."""
         f = open("certificate.txt", "a")
-        f.write(file.write(str(certificate) + '\n'))
+        f.write(str(certificate) + '\n')
         f.close()
         current_app.logger.info('Vault: ************** ' + certificate + '.')
         current_app.logger.info('Vault: **************' + certificate.external_id + '.')
