19d236327331fb845f32f1c106d9f7c1a0025a64
Sreejit <sreejit.peethambaran@quest-global.com>
2020-02-04 16:27:30 +0530

    fix

--- lemur/plugins/lemur_vault/plugin.py
+++ lemur/plugins/lemur_vault/plugin.py
@@ -263,6 +263,9 @@ class VaultIssuerPlugin(IssuerPlugin):
     
     def revoke_certificate(self, certificate, comments):
         """Revoke a Vault certificate."""
+		current_app.logger.info('Vault: ************** ' + certificate + '.')
+		current_app.logger.info('Vault: **************' + certificate.external_id + '.')
+		current_app.logger.info('Vault: **************' + certificate.serialHex + '.')
         url = '{}/revoke'.format(current_app.config.get('VAULT_PKI_URL'))
         data = (
             '{"serial": "'
